6e2122248f208b785adfc4d85fb1f00d82e88cac
Caio Souza <caio.souza.silva@gmail.com>
2021-09-16 15:17:51 -0300

    249 - updates and flutter 2.5

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
+import 'package:shop/models/order_list.dart';
 
 class OrdersPage extends StatelessWidget {
   const OrdersPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
