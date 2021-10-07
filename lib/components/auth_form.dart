b0dde37e491d2fdb3063dcdcd69b1ed0d08fe982
Caio Souza <caio.souza.silva@gmail.com>
2021-10-06 21:55:56 -0300

    create authform

---
+++ lib/components/auth_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({Key? key}) : super(key: key);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Form');
+  }
+}
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -47,7 +48,8 @@ class AuthPage extends StatelessWidget {
                       color: Theme.of(context).textTheme.headline6?.color,
                     ),
                   ),
-                )
+                ),
+                AuthForm(),
               ],
             ),
           )
