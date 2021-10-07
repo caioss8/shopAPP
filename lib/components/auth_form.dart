3a014d499f5ce4d694f0971bfaad7a58004dfc8d
Caio Souza <caio.souza.silva@gmail.com>
2021-10-07 10:01:12 -0300

    implementation validators and AuthMode

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum AuthMode { Signup, Login }
+
 class AuthForm extends StatefulWidget {
   const AuthForm({Key? key}) : super(key: key);
 
@@ -7,7 +9,17 @@ class AuthForm extends StatefulWidget {
   _AuthFormState createState() => _AuthFormState();
 }
 
+void _submit() {}
+
 class _AuthFormState extends State<AuthForm> {
+  AuthMode _authMode = AuthMode.Login;
+  final _passwordController = TextEditingController();
+
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
   @override
   Widget build(BuildContext context) {
     final sizePhone = MediaQuery.of(context).size;
@@ -25,11 +37,58 @@ class _AuthFormState extends State<AuthForm> {
             TextFormField(
               decoration: InputDecoration(labelText: 'Email'),
               keyboardType: TextInputType.emailAddress,
+              onSaved: (email) => _authData['email'] = email ?? '',
+              validator: (_email) {
+                final email = _email ?? '';
+                if (email.trim().isEmpty || !email.contains('@')) {
+                  return 'Insira um email Valido!';
+                } else {
+                  return null;
+                }
+              },
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Senha'),
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
+                decoration: InputDecoration(labelText: 'Senha'),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                controller: _passwordController,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                validator: _authMode == AuthMode.Login
+                    ? null
+                    : (_password) {
+                        final password = _password ?? '';
+                        if (password.isEmpty || password.length < 5) {
+                          return 'Insira uma senha Valida!';
+                        } else {
+                          return null;
+                        }
+                      }),
+            if (_authMode == AuthMode.Signup)
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password != _passwordController.text) {
+                    return 'As senhas precisam ser iguais';
+                  }
+                },
+              ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submit,
+              child: Text(
+                _authMode == AuthMode.Login ? 'Entrar' : 'Resgistrar',
+              ),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 8,
+                  )),
             ),
           ],
         ),
