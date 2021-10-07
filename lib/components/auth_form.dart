b8a04437d5c367cf9c080fd0c70637bc545b49f8
Caio Souza <caio.souza.silva@gmail.com>
2021-10-06 22:00:46 -0300

    implementation textformfield

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -10,6 +10,30 @@ class AuthForm extends StatefulWidget {
 class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
-    return Text('Form');
+    final sizePhone = MediaQuery.of(context).size;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 8,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        height: 320,
+        width: sizePhone.width * 0.75,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Senha'),
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
