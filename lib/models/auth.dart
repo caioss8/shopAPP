d673a09083caf7502174b49eac1947993f77deed
Caio Souza <caio.souza.silva@gmail.com>
2021-10-12 12:10:27 -0300

    change uid for userId

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -6,7 +6,7 @@ import 'package:shop/exceptions/auth_exception.dart';
 class Auth with ChangeNotifier {
   String? _token;
   String? _email;
-  String? _uid;
+  String? _userId;
   DateTime? _expiryDate;
 
   bool get isAuth {
@@ -20,8 +20,8 @@ class Auth with ChangeNotifier {
   String? get email {
     return isAuth ? _email : null;
   }
-  String? get uid {
-    return isAuth ? _uid : null;
+  String? get userID {
+    return isAuth ? _userId : null;
   }
 
   Future<void> _athenticate(
@@ -44,7 +44,7 @@ class Auth with ChangeNotifier {
     }else{
       _token = body['idToken'];
       _email = body['email'];
-      _uid = body['localId'];
+      _userId = body['localId'];
       _expiryDate = DateTime.now().add(
         Duration(
           seconds: int.parse(body['expiresIn']),
