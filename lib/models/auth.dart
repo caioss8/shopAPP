d9ed0a7ffaac12819a3756f0d83b31a27faf3b32
Caio Souza <caio.souza.silva@gmail.com>
2021-10-13 19:02:47 -0300

    método de logout automatico

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String? _email;
   String? _userId;
   DateTime? _expiryDate;
+  Timer? _logoutTimer;
 
   bool get isAuth {
     final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
@@ -17,9 +19,11 @@ class Auth with ChangeNotifier {
   String? get token {
     return isAuth ? _token : null;
   }
+
   String? get email {
     return isAuth ? _email : null;
   }
+
   String? get userId {
     return isAuth ? _userId : null;
   }
@@ -37,11 +41,11 @@ class Auth with ChangeNotifier {
       }),
     );
 
-    final  body = jsonDecode(response.body);
+    final body = jsonDecode(response.body);
 
-    if(body['error'] != null){
+    if (body['error'] != null) {
       throw AuthException(body['error']['message']);
-    }else{
+    } else {
       _token = body['idToken'];
       _email = body['email'];
       _userId = body['localId'];
@@ -50,24 +54,37 @@ class Auth with ChangeNotifier {
           seconds: int.parse(body['expiresIn']),
         ),
       );
+      _autoLogout();
       notifyListeners();
     }
   }
 
   Future<void> signup(String email, String password) async {
     return _athenticate(email, password, 'signUp');
-  } 
+  }
 
   Future<void> login(String email, String password) async {
     return _athenticate(email, password, 'signInWithPassword');
   }
 
-  void logout(){
+  void logout() {
     _token = null;
     _email = null;
     _userId = null;
     _expiryDate = null;
+    _clearLogoutTimer();
     notifyListeners();
   }
 
+  void _clearLogoutTimer(){
+    _logoutTimer?.cancel();
+    _logoutTimer = null;
+  }
+
+  void _autoLogout() {
+    _clearLogoutTimer();
+    final timeToLogout = _expiryDate?.difference(DateTime.now()).inSeconds;
+    print(timeToLogout);
+    _logoutTimer = Timer(Duration(seconds: timeToLogout ?? 0), logout,);
+  }
 }
